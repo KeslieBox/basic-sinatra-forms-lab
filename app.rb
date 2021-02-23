d3f412c2e84f4e41289c2fea0c0d8dc029d98e80
KeslieBox <kesliebox@gmail.com>
2021-02-23 15:38:51 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/newteam' do
+        binding.pry
+        # @team = params[:name]
+        erb :team
+    end
+
+    post '/team' do
+        erb :newteam
+    end
+
 
 end
